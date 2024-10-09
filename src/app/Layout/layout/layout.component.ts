38634019ee67cd2abf4833768f9e2321510dceca
zee-creato <zbigniew.kierpiec@proav.com>
2024-10-09 23:02:51 +0100

    xx

--- src/app/Layout/layout/layout.component.ts
+++ src/app/Layout/layout/layout.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../Header/header/header.component";
 import { RouterOutlet } from '@angular/router';
 import { ToTopComponent } from "./to-top/to-top.component";
-import { SocialAsideComponent } from "../main/social-aside/social-aside.component";
-import { SocialAsideComponent_1 as SocialAsideComponent } from "./social-aside/social-aside.component";
+import { SocialAsideComponent } from './social-aside/social-aside.component';
+
+
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet, ToTopComponent, SocialAsideComponent, SocialAsideComponent_1],
+  imports: [HeaderComponent, RouterOutlet, ToTopComponent, SocialAsideComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
--- src/app/Layout/main/main.component.ts
+++ src/app/Layout/main/main.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { PromiseComponent } from "./promise/promise.component";
 
 
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [SlideshowComponent],
+  imports: [SlideshowComponent, PromiseComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
