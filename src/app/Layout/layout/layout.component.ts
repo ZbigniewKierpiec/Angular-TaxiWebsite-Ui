cac0b79bfa6ac8dc5f54eb2c41cfed50a121eb19
zee-creato <zbigniew.kierpiec@proav.com>
2024-10-10 15:02:47 +0100

    Added footer

--- src/app/Layout/layout/layout.component.ts
+++ src/app/Layout/layout/layout.component.ts
@@ -3,13 +3,14 @@ import { HeaderComponent } from "../../Header/header/header.component";
 import { RouterOutlet } from '@angular/router';
 import { ToTopComponent } from "./to-top/to-top.component";
 import { SocialAsideComponent } from './social-aside/social-aside.component';
+import { FooterComponent } from "./footer/footer.component";
 
 
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet, ToTopComponent, SocialAsideComponent],
+  imports: [HeaderComponent, RouterOutlet, ToTopComponent, SocialAsideComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
