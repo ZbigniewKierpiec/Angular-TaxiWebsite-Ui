4166e5689da0d9b1b9025f60d84d370a609e53e9
zee-creato <zbigniew.kierpiec@proav.com>
2024-10-10 12:26:43 +0100

    Add Components

--- src/app/Layout/main/main.component.ts
+++ src/app/Layout/main/main.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { PromiseComponent } from "./promise/promise.component";
+import { CarsComponent } from "./cars/cars.component";
+import { PlanComponent } from "./plan/plan.component";
 
 
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [SlideshowComponent, PromiseComponent],
+  imports: [SlideshowComponent, PromiseComponent, CarsComponent, PlanComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
